32a8f034cd2a83bcd83ca293de6c7d68c579d1d9
jiwonchoi-phys <chlwldnjs175@gmail.com>
2020-09-16 17:25:30 +0900

    starting menu

--- screen_test.py
+++ screen_test.py
@@ -18,19 +18,32 @@ clock.tick(30)
 
 
 
-def printText(msg,textfont="consolas",size=10,color=BLACK,loc=(0,0)):
-    font = pygame.font.SysFont(textfont,size)
-    text = font.render(msg, True, color)
-    if loc == 'top center': 
-        text_rect = text.get_rect()
-        text_rect.center = (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 4)
-        screen.blit(text,text_rect)
-    elif loc == 'center':
-        text_rect = text.get_rect()
-        text_rect.center = (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
-        screen.blit(text,text_rect)
-    elif loc.type == tuple: # User input of location
-        screen.blit(text,loc)
+class printText():
+    def __init__(self, msg, size, font='consolas', color=BLACK, antialias=True, background=None):
+        self.msg = msg
+        self.font = font
+        self.size = size
+        self.antialias = antialias
+        self.color = color
+        self.background = background 
+        texts = pygame.font.SysFont(self.font, self.size)
+        self.text = texts.render(self.msg, self.antialias, self.color, self.background)
+    
+    def _blit_(self, loc=(0,0)):
+        if loc == 'top center': 
+            text_rect = self.text.get_rect()
+            text_rect.center = (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 4)
+            screen.blit(self.text,text_rect)
+        elif loc == 'center':
+            text_rect = self.text.get_rect()
+            text_rect.center = (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
+            screen.blit(self.text,text_rect)
+        elif loc == 'bottom center':
+            text_rect = self.text.get_rect()
+            text_rect.center = (SCREEN_WIDTH // 2, SCREEN_HEIGHT*3 // 4)
+            screen.blit(self.text,text_rect)
+        elif loc.type == tuple: # User input of location
+            screen.blit(self.text,loc)
 
 
 # Set screen size & background color
@@ -40,6 +53,8 @@ screen.fill(WHITE)
 
 pygame.display.update()
 
+title = printText("Quantum Coda",size=50)        
+new_game = printText("New Game",size=30)
 
 # =============== MAIN LOOP =====================
 while not done:
@@ -49,8 +64,8 @@ while not done:
             done=True
 
         # Starting menu
-        printText("Quantum Coda",size=50,loc='top center')        
-        pygame.draw.rect(screen, BLACK, [75,175,75,50],5)
+        title._blit_(loc='top center')
+        new_game._blit_(loc='bottom center')
         pygame.display.flip()
 
 pygame.quit()
