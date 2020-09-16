ab6a826c584853e345b824169fee96e0617f21c1
jiwonchoi-phys <chlwldnjs175@gmail.com>
2020-09-16 14:41:08 +0900

    color, font, etc.

---
+++ util.py
@@ -0,0 +1,14 @@
+# RGB color information
+
+BLACK   = (  0,  0,  0)
+WHITE   = (255,255,255)
+RED     = (255,  0,  0)
+GREEN   = (  0,255,  0)
+BLUE    = (0  ,  0,255)
+YELLOW  = (255,255,  0)
+CYAN    = (  0,255,255)
+MAGENTA = (255,  0,255)
+
+ORANGE  = (255, 94,  0)
+PURPLE  = (217, 65,197)
+
