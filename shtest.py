61e5fca08ab2ff88458f88df74005983c3a91fbf
gksdid8542 <comflogarden@gmail.com>
2020-11-06 13:47:36 +0900

    주석 및 예시 추가

--- shtest.py
+++ shtest.py
@@ -26,34 +26,46 @@ def shtestroom(): # 조세형 실험실
         pygame.display.update()
 
 def acb1():
-    window = Tk()
-    window.title("버튼 테스트")
-    window.geometry()
-    label = Label(window, text="test")
+    window = Tk()                              # 윈도우 창을 생성
+    window.title("버튼 테스트")                 # 타이틀
+    window.geometry("480x300+100+100")         # "너비x높이+x좌표+y좌표"
+    label = Label(window, text="test")         # 라벨 등록
 
     label.pack()
 
-    b1 = Button(window, text="teeeest")
+
+
+    def b1event():                      # 버튼 함수
+        if(b1['text'] == 'hello'):
+            b1['text'] = 'world'
+            b1['fg'] = 'red'
+            b1['bg'] = 'yellow'
+        else:
+            b1['text'] = 'hello'
+            b1['fg'] = 'yellow'
+            b1['bg'] = 'red'
+        
+    b1 = Button(window, text='hello',command = b1event, fg = 'yellow', bg = 'red')
     b1.pack()
 
-    window.mainloop()
+    window.mainloop()   # 닫기 까지 이게 메인
 
 
 def acb2():
     window=Tk()
     window.title("입력 테스트")
-    window.geometry()
-    window.resizable(False, False)
+    window.geometry("480x300+100+100")
+    window.resizable(False, False)                  # 창 크기 조절 가능 여부 거부
     label = Label(window, text="흰창에 숫자 입력")
 
     def calc(event):
-        label.config(text="결과="+str(eval(entry.get())))
-        print(str(eval(entry.get())))
+        label.config(text="결과="+str(eval(entry.get()))) # 라벨 수정
+        print(str(eval(entry.get())))                     # 기입값 출력
 
-    entry=Entry(window)
-    entry.bind("<Return>", calc)
-    entry.pack()
+    entry=Entry(window, bd = 30)      # 기입창, 크기 기본 위아래폭의 30배
+    entry.bind("<Return>", calc)      # 리턴값 calc 함수에 사용
+    entry.pack(pady = 10)             # 위아래 간격 10
 
-    label.pack()
+    label.pack(pady = 10)
 
     window.mainloop()
