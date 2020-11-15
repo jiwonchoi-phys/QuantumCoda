abb587bb47efb9e1c30ff3608f654fccea7ab07a
Luminebear <rainstar77@outlook.com>
2020-11-15 14:43:55 +0900

    Changed font

--- util.py
+++ util.py
@@ -48,7 +48,7 @@ max_card_num = 10 # 13까지 가능하나 10 완성 전까지 고정할 것. mak
 class PRINTTEXT():
 
 
-    def __init__(self, msg, size, font='calibri', color=BLACK, antialias=True, background=None):
+    def __init__(self, msg, size, font='notosanscjkkr', color=BLACK, antialias=True, background=None):
         self.msg = msg                  # 메세지
         self.font = font                # font 지정 (기본 conslas)
         self.size = size                # size 지정
