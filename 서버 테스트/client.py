83cf7e0af7bc7b5b9c86e85085222926be8af297
gksdid8542 <comflogarden@gmail.com>
2020-11-25 20:37:36 +0900

    test

---
+++ 서버 테스트/client.py
@@ -0,0 +1,36 @@
+import pygame
+from network import Network
+from player import Player
+
+width = 700
+height = 500
+win = pygame.display.set_mode((width, height))
+pygame.display.set_caption("Client")
+
+
+def redrawWindow(win,player, player2): # draw player 1 and player 2 in pygame 
+    win.fill((255,255,255))
+    player.draw(win)
+    player2.draw(win)
+    pygame.display.update()
+
+def main():
+    run = True
+    n = Network()
+    p = n.getP()
+    clock = pygame.time.Clock()
+
+    while run:
+        clock.tick(60)
+        p2 = n.send(p)
+ 
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                pygame.quit()
+
+        p.move()
+        p.onpress()
+        redrawWindow(win, p, p2)
+
+main()
---
+++ 서버 테스트/server.py
@@ -0,0 +1,62 @@
+import socket
+from _thread import *
+from player import Player
+import pickle
+
+server = "localhost"        # server IP
+port = 5555                 # server Port
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # use IPv4 for connection and process any datas as Binary (TCP)
+
+try:
+    s.bind((server, port)) # get server ip and port
+except socket.error as e:
+    str(e)
+
+s.listen(3)
+print("Waiting for a connection, Server Started")
+
+
+players = [Player(0,0,50,50,(255,0,0)), Player(100,100, 50,50, (0,0,255))] # Server arranges players.
+
+
+def threaded_client(conn, player):
+    conn.send(pickle.dumps(players[player]))
+    reply = ""
+    while True:
+        try:
+            data = pickle.loads(conn.recv(2048)) # recv("packet"). lower to 2K(1K = 1024 bytes) max to 64K. If the data size is 8K and your buffer size is 2K then the data will be process to divide 4 and 2K*4
+            players[player] = data
+
+            if not data:
+                print("Disconnected")
+                break
+            else:
+                print(players[0].g_ready())
+                print(players[1].g_ready())
+                if player == 1:
+                    reply = players[0]
+                else:
+                    reply = players[1]
+                
+                if players[0].g_ready() == True and players[1].g_ready() == True:
+                    players[0].aa()
+                    players[1].aa()
+                    print("all ready")
+                #print("Received: ", data)
+                #print("Sending : ", reply)
+
+            conn.sendall(pickle.dumps(reply))
+        except:
+            break
+
+    print("Lost connection")
+    conn.close()
+
+currentPlayer = 0
+while True:
+    conn, addr = s.accept()
+    print("Connected to:", addr)
+
+    start_new_thread(threaded_client, (conn, currentPlayer))
+    currentPlayer += 1
