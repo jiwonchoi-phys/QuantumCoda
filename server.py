f130c234af747cbe46887215302408c4668003b1
gksdid8542 <comflogarden@gmail.com>
2020-11-28 12:09:27 +0900

    1

--- server.py
+++ server.py
@@ -4,59 +4,6 @@ from player import Player
 from tkinter import *
 import pickle
 
-def f_room(room):
-    server = "localhost"        # server IP
-    port = 5554+room                # server Port
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # use IPv4 for connection and process any datas as Binary (TCP)
-    try:
-        s.bind((server, port)) # get server ip and port
-    except socket.error as e:
-        str(e)
-    s.listen(3)
-    print("Waiting for a connection, Server Started")
-
-    players = [Player(100,100,100,100,(255,0,0)), Player(300,100,100,100, (0,0,255))] # Server arranges players.
-
-    def threaded_client(conn, player):
-        conn.send(pickle.dumps(players[player]))
-        reply = ""
-        while True:
-            try:
-                data = pickle.loads(conn.recv(2048)) # recv("packet"). lower to 2K(1K = 1024 bytes) max to 64K. If the data size is 8K and your buffer size is 2K then the data will be process to divide 4 and 2K*4
-                players[player] = data
-
-                if not data:
-                    print("Disconnected")
-                    break
-                else:
-                    
-                    if players[0].g_ready() == True and players[1].g_ready() == True:
-                        #players[0].aa()
-                        #players[1].aa()
-                        print("all ready")
-
-                    if player == 1:
-                        reply = players[0]
-                    else:
-                        reply = players[1]
-                    
-                    #print("Received: ", data)
-                    #print("Sending : ", reply)
-
-                conn.sendall(pickle.dumps(reply))
-            except:
-                break
-
-        print("Lost connection")
-        conn.close()
-
-    currentPlayer = 0
-    while True:
-        conn, addr = s.accept()
-        print("Connected to:", addr)
-        start_new_thread(threaded_client, (conn, currentPlayer))
-        currentPlayer += 1
-
 def f_rn(): # 룸 넘버 입력 받는 tk 임시.
     global room
     rn_tk=Tk()
@@ -85,6 +32,65 @@ def f_rn(): # 룸 넘버 입력 받는 tk 임시.
     label.pack()
 
     rn_tk.mainloop()
+
+
+
+'''
+===========================================서버=========================================
+'''
+
+
+
+def f_room(room):
+    server = "localhost"        # server IP
+    port = 5554+room                # server Port
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # use IPv4 for connection and process any datas as Binary (TCP)
+    try:
+        s.bind((server, port)) # get server ip and port
+    except socket.error as e:
+        str(e)
+    s.listen(3)
+    print("Waiting for a connection, Server Started")
+
+    players = [Player(100,100,100,100,(255,0,0)), Player(300,100,100,100, (0,0,255))] # Server arranges players.
+
+    def threaded_client(conn, player):
+        conn.send(pickle.dumps(players[player]))
+        reply = ""
+        while True:
+            try:
+                data = pickle.loads(conn.recv(2048)) # recv("packet"). lower to 2K(1K = 1024 bytes) max to 64K. If the data size is 8K and your buffer size is 2K then the data will be process to divide 4 and 2K*4
+                players[player] = data
+
+                if not data:
+                    print("Disconnected")
+                    break
+                else:
+                    
+                    if players[0].g_ready() == True and players[1].g_ready() == True:
+                        print("all ready")
+
+                    if player == 1:
+                        reply = players[0]
+                    else:
+                        reply = players[1]
+                    
+                    #print("Received: ", data)
+                    #print("Sending : ", reply)
+
+                conn.sendall(pickle.dumps(reply))
+            except:
+                break
+
+        print("Lost connection")
+        conn.close()
+
+    currentPlayer = 0
+    while True:
+        conn, addr = s.accept()
+        print("Connected to:", addr)
+        start_new_thread(threaded_client, (conn, currentPlayer))
+        currentPlayer += 1
 
 f_rn()
 f_room(room)
